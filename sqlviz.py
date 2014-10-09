c4e56b2816ded71eaa5faccc876beae2d7441477
Hawk Weisman <hawk@meteorcodelabs.com>
2014-10-09 17:15:19 -0400

    Add a class skeleton for the Schema class None of the methods defined here actually do anything, they're just so that I can write a test suite to expect the correct things.

---
+++ sqlviz.py
@@ -0,0 +1,28 @@
+#! usr/bin/env python3
+
+class Schema:
+    """
+    Wraps the SQL source code for a schema and provides methods to get information about that schema.
+    """
+
+    def __init__(self, source):
+        """
+        Creates a new instance of Schema for the specified source code string.
+        """
+        self.source = source
+
+    def n_tables(self):
+        """ Returns the number of tables defined in the schema"""
+        pass
+
+    def n_keys(self):
+        """Returns the number of keys defined in the schema"""
+        pass
+
+    def n_datatypes(self):
+        """Returns the number of each data type in the schema."""
+        pass
+
+    def lengths(self):
+        """Returns a dictionary mapping each data type in the schema to a list of the lengths of those data types in the schema."""
+        pass
