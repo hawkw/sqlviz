653ab072c4440dac4ce86e354107ada6c98d4067
Hawk Weisman <hawk.weisman@gmail.com>
2014-10-10 13:16:48 -0400

    Update tests.py

--- tests.py
+++ tests.py
@@ -6,5 +6,9 @@ import sqlviz
 
 # Tests will go here...eventually
 
+# I'm considering using doctest (https://docs.python.org/3/library/doctest.html#module-doctest)
+# rather than unittest for some/all of the testing since it seems to be both easier and more semantic.
+# It also forces us to write specs in documentation, which I for one think is kind of awesome.
+
 if __name__ == '__main__':
     unittest.main()
