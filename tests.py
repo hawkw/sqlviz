d861f71d9c5ce51f5498d422a5d2d60da40e9222
Hawk Weisman <hawk.weisman@gmail.com>
2014-10-11 14:55:38 -0400

    Quick test suite to describe all Schema behavior
    Everything is currently unimpemented, so all these tests should initially fail. Test against the Inventory schema provided by Dr. Kapfhammer.

--- tests.py
+++ tests.py
@@ -2,13 +2,39 @@
 
 
 import unittest
-import sqlviz
+from sqlviz import Schema
 
 # Tests will go here...eventually
 
-# I'm considering using doctest (https://docs.python.org/3/library/doctest.html#module-doctest)
-# rather than unittest for some/all of the testing since it seems to be both easier and more semantic.
-# It also forces us to write specs in documentation, which I for one think is kind of awesome.
+class InventorySchemaSpec (unittest.TestCase):
+    
+    def setUp (self):
+        self.schema = Schema(
+            """DROP TABLE Inventory;
+
+            CREATE TABLE Inventory
+            (
+            id INT PRIMARY KEY,
+            product VARCHAR(50) UNIQUE,
+            quantity INT,
+            price DECIMAL(18,2)
+            );""")
+            
+    def test_n_tables(self):
+        self.assertEqual(self.schema.n_tables(),  1,
+                        "The Inventory schema should contain 1 table.")
+                        
+    def test_n_keys(self):
+        self.assertEqual(self.schema.n_keys(),  {"PRIMARY KEY": 1, "FOREIGN KEY:" 0},
+                        "The Inventory schema should contain 1 primary key and 0 foreign keys.")
+                        
+    def test_n_datatypes(self):
+        self.assertEqual(self.schema.n_datatypes(), {"INT": 2, "VARCHAR": 1, "DECIMAL": 1},
+                        "The Inventory schema should contain two INTs, one VARCHAR, and one DECIMAL")
+    
+    def lengths(self):
+        self.assertEqual(self.schema.lengths(), {"VARCHAR": [50], "DECIMAL": [(18,2)]},
+                        "The Inventory schema should contain one VARCHAR(50) and one DECIMAL(18,2)")
 
 if __name__ == '__main__':
     unittest.main()
