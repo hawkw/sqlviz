b6b65c18b8ad911a7b43be95aae24e3e5b839562
Hawk Weisman <hawk@meteorcodelabs.com>
2014-10-11 17:51:31 -0400

    Made the test for lengths actually a test [ci-skip]

--- tests.py
+++ tests.py
@@ -7,7 +7,7 @@ from sqlviz import Schema
 # Tests will go here...eventually
 
 class InventorySchemaSpec (unittest.TestCase):
-    
+
     def setUp (self):
         self.schema = Schema(
             """DROP TABLE Inventory;
@@ -19,20 +19,20 @@ class InventorySchemaSpec (unittest.TestCase):
             quantity INT,
             price DECIMAL(18,2)
             );""")
-            
+
     def test_n_tables(self):
         self.assertEqual(self.schema.n_tables(),  1,
                         "The Inventory schema should contain 1 table.")
-                        
+
     def test_n_keys(self):
         self.assertEqual(self.schema.n_keys(),  {"PRIMARY KEY": 1, "FOREIGN KEY": 0},
                         "The Inventory schema should contain 1 primary key and 0 foreign keys.")
-                        
+
     def test_n_datatypes(self):
         self.assertEqual(self.schema.n_datatypes(), {"INT": 2, "VARCHAR": 1, "DECIMAL": 1},
                         "The Inventory schema should contain two INTs, one VARCHAR, and one DECIMAL")
-    
-    def lengths(self):
+
+    def test_lengths(self):
         self.assertEqual(self.schema.lengths(), {"VARCHAR": [50], "DECIMAL": [(18,2)]},
                         "The Inventory schema should contain one VARCHAR(50) and one DECIMAL(18,2)")
 
